4da5d3f704a718cf33257fd0c987b25535b0d380
James Michael <jomichael1994@gmail.com>
2024-02-08 20:28:17 +0000

    use styled components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,53 +1,62 @@
 import { useState } from 'react';
+import { styled } from 'styled-components';
+
+import Button from './Button';
+import Input from './Input';
+
+const ControlContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+	const [enteredEmail, setEnteredEmail] = useState('');
+	const [enteredPassword, setEnteredPassword] = useState('');
+	const [submitted, setSubmitted] = useState(false);
 
-  function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
-      setEnteredEmail(value);
-    } else {
-      setEnteredPassword(value);
-    }
-  }
+	function handleInputChange(identifier, value) {
+		if (identifier === 'email') {
+			setEnteredEmail(value);
+		} else {
+			setEnteredPassword(value);
+		}
+	}
 
-  function handleLogin() {
-    setSubmitted(true);
-  }
+	function handleLogin() {
+		setSubmitted(true);
+	}
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
-  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+	const emailNotValid = submitted && !enteredEmail.includes('@');
+	const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
-  return (
-    <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-      </div>
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
-      </div>
-    </div>
-  );
+	return (
+		<div id="auth-inputs">
+			<ControlContainer>
+				<Input
+					label="Email"
+					type="email"
+					invalid={emailNotValid}
+					onChange={(event) =>
+						handleInputChange('email', event.target.value)
+					}
+				/>
+				<Input
+					label="Password"
+					type="password"
+					invalid={passwordNotValid}
+					onChange={(event) =>
+						handleInputChange('email', event.target.value)
+					}
+				/>
+			</ControlContainer>
+			<div className="actions">
+				<button type="button" className="text-button">
+					Create a new account
+				</button>
+				<Button onClick={handleLogin}>Sign In</Button>
+			</div>
+		</div>
+	);
 }
